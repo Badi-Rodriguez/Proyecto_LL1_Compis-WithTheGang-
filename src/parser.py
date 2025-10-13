bf9e41bb9532e45fb8ca0a35caea9ea3dddba659
lajesfen <luciano.aguirre@utec.edu.pe>
2025-10-13 14:40:36 -0500

    add: Initial LR(1) Parser classes

--- src/parser.py
+++ src/parser.py
@@ -1,8 +1,18 @@
-from src.scanner import Grammar
+from src.scanner import Symbol, Production, Grammar
+from typing import List, Dict
+
+class GrammarRuleLR():
+    def __init__(self, start_symbol: Symbol, production: Production, search_symbol: Symbol):
+        self.start_symbol: Symbol = start_symbol
+        self.production: Production = production
+        self.search_symbol: Symbol = search_symbol
+        self.dot_pos: int = 0
 
 class State:
     def __init__(self):
-        return
+        self.id: int = 0
+        self.rules: List[GrammarRuleLR]
+        self.transitions: List[Dict[Symbol, State]]
 
 class LR1:
     def __init__(self, grammar):
